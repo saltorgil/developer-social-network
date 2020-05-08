c619342b665414320a923a47470156264c984ff3
Salva Torru <salva339@gmail.com>
2020-05-08 10:31:11 +0100

    added Profile model, auth and profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,101 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      res.status(400).send({ msg: 'Profile not found' });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/profile/me
+// @desc    Create or update user profile
+// @access  Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').notEmpty(),
+      check('skills', 'Skills is required').notEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body;
+
+    // Build profile object
+
+    const profileFields = {
+      user: req.user.id,
+      company,
+      location,
+      website: website,
+      bio,
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(',').map((skill) => skill.trim()),
+      status,
+      githubusername,
+    };
+
+    // Build Social object
+
+    profileFields.social = { youtube, twitter, instagram, linkedin, facebook };
+
+    console.log(profileFields.skills);
+    console.log(profileFields.social);
+
+    try {
+      let query = { user: req.user.id };
+      let options = { upsert: true, new: true, setDefaultsOnInsert: true };
+      let profile = await Profile.findOneAndUpdate(
+        query,
+        profileFields,
+        options
+      );
+      return res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
