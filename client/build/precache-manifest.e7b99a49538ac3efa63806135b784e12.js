fcfe273ffad1d281a97d8dea74460bba5619fab7
Salva Torru <salva339@gmail.com>
2020-06-11 16:01:35 +0100

    built

---
+++ client/build/precache-manifest.e7b99a49538ac3efa63806135b784e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4121e0572849e6bd072e9b83c7a8fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c471974fdccc303b80c1",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "e92e7c08a34c4f81985e",
+    "url": "/static/js/2.5328b965.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.5328b965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c471974fdccc303b80c1",
+    "url": "/static/js/main.9b681076.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
